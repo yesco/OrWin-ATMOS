a5e71505e47043710b8b9edd6ed939dfe35ba4cf
Jonas S Karlsson <jsk@yesco.org>
2026-08-15 14:23:57 +0700

    a compiling version of shell.c, not tested

--- shell.c
+++ shell.c
@@ -1,5 +1,14 @@
 // OrWIN Shell pipeline execute
 
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#include <stdio.h>
+
+#define EOS ((char*)-1)
+
 char* cmdnames=
   "iota "
   "ls cat find "
@@ -20,34 +29,41 @@ char* cmdnames=
   // "curl wget rsync scp "
   // "ping ip ss netstat "
   // "git "
-  
-};
 
-typedef char* (*cmdfun)(void* state, char* line);
+  ;
+
+typedef void* (*cmdfun)(void* state, char* line);
 
 cmdfun commands[]= {
 };
 
-typedef *cmdfun cmdtrain;
+typedef cmdfun* cmdtrain;
 
-struct simplestate { cmdfun f; }
+typedef struct simplestate { cmdfun f; } simplestate;
 
-void* stalloc(unsigned int size, cmdfun f) {
+void* stalloc(unsigned int size, void* f) {
   simplestate* state= calloc(size, 1);
-  state->f= pwd;
+  state->f= f;
   return state;
 }
 
 #define STALLOC(strct, fun) stalloc(sizeof(strct), fun)
 
-struct pstate { cmdfun f; char* s; };
+typedef struct pstate { cmdfun f; char* s; } pstate;
 
-#define PSTALLOC(fun, p) (state=STALLOC(pstate, fun), state->p, state)
+#define PSTALLOC(fun, p) (state=STALLOC(pstate, fun), state->s=p, state)
 
+#if !defined(_POSIX_C_SOURCE) && !defined(__ANDROID__) && !defined(_STRDUP_DEFINED)
+  #define strdup(s) safe_fallback_strdup(s)
+  
+  static char* safe_fallback_strdup(const char* s) {
+      if (!s) return NULL;
+      size_t len = strlen(s) + 1;
+      char* d = malloc(len);
+      return d ? memcpy(d, s, len) : NULL;
+  }
+#endif
 
-char* strdup(char* s) {
-  return !s? s: strcpy(calloc(strlen(s)+1, 1), s);
-}
 
 void* lfree(char* line) {
   // TODO: keep a pool? reuse!
@@ -75,11 +91,11 @@ void* pwd(simplestate* state, char* line) {
 }
 
 
-void* grep(strstate* state, char* line) {
+void* grep(pstate* state, char* line) {
   if (!state) return PSTALLOC(grep, line);
 
   // pass-through backtracking
-  if (!line || line==EOF) return line;
+  if (!line || line==EOS) return line;
 
   // match one line
   return strstr(line, state->s)? line: lfree(line);
@@ -89,60 +105,83 @@ void* grep(strstate* state, char* line) {
 // fake file
 char* fakefile[]= { "one", "two", "three", "four", "five", NULL };
 
-struct fakefilestate { cmdfun f; char** fil; }
+typedef struct fakefilestate { cmdfun f; char** fil; } fakefilestate;
 
 void* cat(fakefilestate* state, char* line) {
-  if (!state) return PSTALLOC(cat, fakefile);
+  if (!state) {
+    state= STALLOC(fakefilestate, cat);
+    state->fil= fakefile;
+    return state;
+  }
 
   // pass on
-  if (!line || line==EOF) return line;
+  if (!line || line==EOS) return line;
 
   return lstrcpy(line, *state->fil++);
 }
   
 
-struct wcstate { cmdfun f; unsigned int ln, wn, cn; };
+typedef struct wcstate { cmdfun f; unsigned int ln, wn, cn; } wcstate;
 
-void* wc(wcstate* st, char* line) {
+void* wc(wcstate* state, char* line) {
   char c, *s= line;
   unsigned int n;
   
-  if (!st) return STALLOC(wcstatede, wc);
+  if (!state) return STALLOC(wcstate, wc);
 
   // Output summary at end of file
-  if (line==EOF) {
+  if (line==EOS) {
     line= malloc(25);
-    sprintf(line, "%u %u %u", wcstate->ln, wcstate->wn, wcstate->cn);
+    sprintf(line, "%u %u %u", state->ln, state->wn, state->cn);
     return line;
     // TODO: do we need to put code to give EOF?
   }
 
   // process one line
-  st->ln++;
-  while((c==*s)) {
-    while(isspace(c)) c== *++s,++n;
-    if (c) st->wn++;
-    while(c && !isspace(c)) c== *++s,++n;
+  state->ln++;
+  while((c=*s)) {
+    while(isspace(c)) c=*++s,++n;
+    if (c) state->wn++;
+    while(c && !isspace(c)) c=*++s,++n;
   }
-  wc->cn+= n;
+  state->cn+= n;
   
   // returns null (backtracks to get prev line)
   return lfree(line);
 }
   
+// more like "tee -"
+void* teeterminal(simplestate* state, char* line) {
+  if (!state) return STALLOC(wcstate, wc);
+  if (line==EOS) puts("*EOS*");
+  else if (line) puts(line);
+  else puts("*NULL*");
+  return line;
+}
 
-void sexec(cmdtrain *train) {
+// more like "tee -"
+void* terminal(simplestate* state, char* line) {
+  if (!state) return STALLOC(wcstate, wc);
+  if (line==EOS) puts("*EOS*");
+  else if (line) puts(line);
+  else puts("*NULL*");
+  // force backtracking
+  return NULL;
+}
+
+
+void wsystrain(cmdtrain *train) {
   cmdfun *fp;
-  char* line= EOF;
+  char* line= EOS;
 
-  while((fp==*train)) {
+  while((fp=*train)) {
     line= (*fp)(fp, line);
     if (line) ++fp; else --fp;
   }    
 }
 
 
-int system(char* cmd) {
+int wsystem(char* cmd) {
   cmdtrain mock[]= {
     0,
     pwd(0, 0),
@@ -151,77 +190,12 @@ int system(char* cmd) {
   };
   
   // Error codes? How & semantics
-  sexec(&mock);
+  wsystrain(mock);
 
   return 0;
 }
 
-/*
-
-[15.2%] cd
-[13.8%] ls
-[8.4%] grep
-[6.5%] cat
-[5.1%] rm
-[4.8%] cp
-[4.6%] mv
-[3.5%] ssh
-[2.8%] echo
-[2.5%] mkdir
-[2.2%] find
-[2.1%] less
-[1.9%] clear
-[1.8%] pwd
-[1.6%] sudo
-[1.4%] chmod
-[1.3%] chown
-[1.2%] curl
-[1.1%] tar
-[1.0%] tail
-[0.9%] ps
-[0.8%] top
-[0.8%] htop
-[0.7%] df
-[0.7%] du
-[0.6%] kill
-[0.6%] history
-[0.5%] git
-[0.5%] sed
-[0.5%] awk
-[0.5%] wget
-[0.4%] ping
-[0.4%] touch
-[0.4%] date
-[0.4%] head
-[0.4%] diff
-[0.3%] sort
-[0.3%] uniq
-[0.3%] wc
-[0.3%] xargs
-[0.3%] rsync
-[0.3%] scp
-[0.2%] free
-[0.2%] ip
-[0.2%] ss
-[0.2%] ln
-[0.2%] cut
-[0.2%] man
-[0.2%] whoami
-[0.1%] uptime
-[0.1%] uname
-[0.1%] gzip
-[0.1%] gunzip
-[0.1%] zip
-[0.1%] unzip
-[0.1%] killall
-[0.1%] tr
-[0.1%] basename
-[0.1%] dirname
-[0.1%] rmdir
-[0.05%] paste
-[0.05%] comm
-[0.05%] netstat
-[0.05%] chgrp
-
-
- */
+int main(int argc, char** argv) {
+  wsystem("pwd | terminal");
+  return 0;
+}
