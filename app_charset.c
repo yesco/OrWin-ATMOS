89fee629dfada662aecff1bf1d949c0ba0c6a8b2
Jonas S Karlsson <jsk@yesco.org>
2026-08-25 22:10:06 +0700

    charset: fixed last wraparound line clear first, lol

--- app_charset.c
+++ app_charset.c
@@ -11,14 +11,14 @@ void* app_charset(APP* app, char* line) {
   if (!app) {
     int c;
 
-    window(-1, -1, 18, 11, green, black);
+    window(-1, -1, 18, 12, green, black);
     wstatus(-1, "Charset (edit)");
     
-    gotoxy(0, 0);
     for(c= 0; c<256; ++c) {
       if ((c & 0x7f) < 32) continue;
       // beginning of line
       if ((c & 0b1111)==0) putcraw(c<=128? 8: 9);
+      // ORIC only have 80 alt chars (rest overlaps w screen!)
       if (c < 128+32+80) putcraw(c & 0x7f);
       // end of line
       if ((c & 0b1111)==0b1111) { putcraw(8); putchar(' '); }
